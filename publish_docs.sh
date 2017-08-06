dd40bf63aefecaaca4e38604b03501c7b734cad7
Endel Dreyer <endel.dreyer@gmail.com>
2017-08-06 17:19:43 +0200

    update publish docs script

--- publish_docs.sh
+++ publish_docs.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
-
 yarn docs
 cd docs
 git init .
 git remote add origin git://github.com/gamestdio/mathf.git
 git checkout -b docs
 git add .
+git commit -m "update docs"
 git push origin docs:gh-pages
